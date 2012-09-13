4612985027d743e3ec8817e37f47aaf0c12ae2ee
Urban <usoban@urban-toshiba-p300.(none)>
2012-09-13 14:55:39 +0200

    updated header

--- moviesio_imdb_import.js
+++ moviesio_imdb_import.js
@@ -1,10 +1,5 @@
 /*
- * This is a JavaScript Scratchpad.
- *
- * Enter some JavaScript, then Right Click or choose from the Execute Menu:
- * 1. Run to evaluate the selected text,
- * 2. Inspect to bring up an Object Inspector on the result, or,
- * 3. Display to insert the result in a comment after the selection.
+ * IMDB watchlist importer for movies.io
  */
 
 (function(){
